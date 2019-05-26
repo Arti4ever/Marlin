d22289ac388a8e78b0102a7d248da4d4d6494d11
Arti4ever <39329296+Arti4ever@users.noreply.github.com>
2019-05-26 20:29:56 +0200

    Revert "jump to 256 native steps"
    This reverts commit fafec718bd7ee1d6f87b8d288ee423cbc6f92c93.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1545,11 +1545,11 @@
 #if HAS_TRINAMIC
 
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
-  #define INTERPOLATE      false  // Interpolate X/Y/Z_MICROSTEPS to 256
+  #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT     534  // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_MICROSTEPS  256  // 0..256
+    #define X_MICROSTEPS   16  // 0..256
     #define X_RSENSE     0.11
   #endif
 
@@ -1561,7 +1561,7 @@
 
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT     534
-    #define Y_MICROSTEPS  256
+    #define Y_MICROSTEPS   16
     #define Y_RSENSE     0.11
   #endif
 
@@ -1573,7 +1573,7 @@
 
   #if AXIS_IS_TMC(Z)
     #define Z_CURRENT     534
-    #define Z_MICROSTEPS  256
+    #define Z_MICROSTEPS   16
     #define Z_RSENSE     0.11
   #endif
 
@@ -1591,7 +1591,7 @@
 
   #if AXIS_IS_TMC(E0)
     #define E0_CURRENT    636
-    #define E0_MICROSTEPS 256
+    #define E0_MICROSTEPS  16
     #define E0_RSENSE    0.11
   #endif
 
